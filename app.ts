bc9ce4ceaf4174b63579a567a4ae648e43b57523
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-02 20:39:15 -0300

    feat: add flight routes in app.ts

--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import { indexRouter } from './src/routes';
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+app.use(indexRouter);
 
 app.use(express.urlencoded({ extended: true }));
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import { flightRouter } from './flightRoutes';
 
-export const router = Router();
+const router = Router();
 
 router.use('/flights', flightRouter);
+
+export { router as indexRouter };
