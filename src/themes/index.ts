f6b23e54280a559e73e0af706ed3340a6a01df1f
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-03 19:46:51 -0300

    feat: add new default themes

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,27 +1,28 @@
 export const themes = {
-    COLORS: {
-        PRIMARY_COLOR: '#7e62ae',
-        BLACK: '#000000',
-        WHITE: '#FFFFFF',
-        GRAY: {
-            LIGHT: '#f0f0f0',
-            MEDIUM: '#b0b0b0',
-            DARK: '#707070',
-        },
+  COLORS: {
+    PRIMARY_COLOR: '#7e62ae',
+    PRIMARY_COLOR_LIGHT: '#a694c8',
+    BLACK: '#000000',
+    WHITE: '#FFFFFF',
+    GRAY: {
+      LIGHT: '#f0f0f0',
+      MEDIUM: '#b0b0b0',
+      DARK: '#707070',
     },
-    FONTS: {
-        PRIMARY: 'Montserrat, sans-serif',
-        SECONDARY: 'Roboto, sans-serif',
-    },
-    FONT_SIZE: {
-        SMALL: '12px',
-        MEDIUM: '16px',
-        LARGE: '20px',
-    },
-    FONT_WEIGHT: {
-        LIGHT: 300,
-        REGULAR: 400,
-        MEDIUM: 500,
-        BOLD: 700,
-    },
+  },
+  FONTS: {
+    PRIMARY: 'Montserrat, sans-serif',
+    SECONDARY: 'Roboto, sans-serif',
+  },
+  FONT_SIZE: {
+    SMALL: '12px',
+    MEDIUM: '16px',
+    LARGE: '20px',
+  },
+  FONT_WEIGHT: {
+    LIGHT: 300,
+    REGULAR: 400,
+    MEDIUM: 500,
+    BOLD: 700,
+  },
 }
