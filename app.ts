aaf2e3ba25287faf17b3e5b941a15cbdf3effc08
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-02 21:11:08 -0300

    feat: add database connection test in app.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
-import express from 'express';
-import { Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import { Request, Response } from 'express';
+
 import { indexRouter } from './src/routes';
+import { PrismaClientSingleton } from './src/config/db/PrismaClientSingleton';
 
 dotenv.config();
 
@@ -9,9 +11,17 @@ const app = express();
 
 app.use(express.json());
 
-app.use(indexRouter);
-
 app.use(express.urlencoded({ extended: true }));
+
+PrismaClientSingleton.getInstance().$connect().then(() => {
+  console.log('Connected to the database successfully');
+}
+).catch((error) => {
+  console.error('Database connection failed:', error);
+  process.exit(1);
+});
+
+app.use(indexRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
