d6876604123a1b67c660e0f4706381184d1b594e
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-03 20:04:06 -0300

    feat: create FlightsFindAll page with styled container and header

---
+++ src/pages/Flights/FlightsFindAll/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from './../../../components/Header'
+
+import { FlightsFindAllContainer } from './styles'
+
+export const FlightsFindAll = () => {
+  return (
+    <FlightsFindAllContainer>
+      <Header title='Buscar voos' />
+    </FlightsFindAllContainer>
+  )
+}
---
+++ src/pages/Flights/FlightsFindAll/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const FlightsFindAllContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  flex: 1;
+  height: 100%;
+`
