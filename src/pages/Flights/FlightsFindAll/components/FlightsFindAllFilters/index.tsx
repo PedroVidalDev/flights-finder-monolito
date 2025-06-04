98e1616416a23bc62804658f786379cf0b891c4b
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-04 01:54:45 -0300

    feat: implement filtering inputs get value and state management in FlightsFindAllFilters component

--- src/pages/Flights/FlightsFindAll/components/FlightsFindAllFilters/index.tsx
+++ src/pages/Flights/FlightsFindAll/components/FlightsFindAllFilters/index.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import { Input } from 'components/Input'
+
 import {
   FilterButton,
   FlightsFindAllFiltersContainer,
@@ -7,15 +10,42 @@ import {
 } from './style'
 
 export const FlightsFindAllFilters = () => {
+  const [origin, setOrigin] = useState('')
+  const [destination, setDestination] = useState('')
+  const [departureDate, setDepartureDate] = useState('')
+
+  const handleFilter = () => {
+    // Implement filter logic here
+    console.log('Filtering flights with:', {
+      origin,
+      destination,
+      departureDate,
+    })
+  }
+
   return (
     <FlightsFindAllFiltersContainer>
       <FlightsFindAllFiltersTitle>Filtros</FlightsFindAllFiltersTitle>
       <InputsContainer>
-        <Input type='text' label='Origem' />
-        <Input type='text' label='Destino' />
-        <Input type='date' label='Data de partida' />
+        <Input
+          type='text'
+          label='Origem'
+          onChange={(e) => setOrigin(e.target.value)}
+        />
+
+        <Input
+          type='text'
+          label='Destino'
+          onChange={(e) => setDestination(e.target.value)}
+        />
+
+        <Input
+          type='date'
+          label='Data de partida'
+          onChange={(e) => setDepartureDate(e.target.value)}
+        />
       </InputsContainer>
-      <FilterButton>Filtrar</FilterButton>
+      <FilterButton onSubmit={handleFilter}>Filtrar</FilterButton>
     </FlightsFindAllFiltersContainer>
   )
 }
