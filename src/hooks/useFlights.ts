7836ddb95c8e93c9dffdd9927dc3fdb6baa79af4
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-05 12:21:12 -0300

    refactor: simplify page handling in useFlights hook by removing unused state and renaming parameters

--- src/hooks/useFlights.ts
+++ src/hooks/useFlights.ts
@@ -7,23 +7,21 @@ import { api } from 'services/axios'
 
 export const useFlights = () => {
   const [flights, setFlights] = useState<FlightDTO[]>([])
-  const [page, setPage] = useState<number>(1)
   const [maxPage, setMaxPage] = useState<number>(1)
 
   const fetchGetAllFlights = useCallback(
     async (
-      newPage?: number | null,
+      page?: number | null,
       filters?: { field: string; value: string }[],
     ) => {
       const response = await api.get<PaginationDTO<FlightDTO[]>>('/flights', {
         params: {
-          page: newPage ?? page,
+          page: page,
           limit: 10,
           filters: JSON.stringify(filters) ?? undefined,
         },
       })
       setFlights(response.data.data?.data ?? [])
-      setPage(response.data.data?.page ?? 1)
 
       const total = response.data.data?.total
       const limit = response.data.data?.limit
