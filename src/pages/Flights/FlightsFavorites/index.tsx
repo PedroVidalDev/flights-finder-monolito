c5d062c456673f57d075907310bdef9d9089ab66
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-05 12:18:29 -0300

    feat: enhance FlightsFavorites page to dynamically display favorite flights from storage

--- src/pages/Flights/FlightsFavorites/index.tsx
+++ src/pages/Flights/FlightsFavorites/index.tsx
@@ -3,8 +3,32 @@ import { Header } from 'components/Header'
 import { FlightsFavoritesContainer, FlightsFindAllTable } from './styles'
 import { Table } from 'components/Table'
 import { flightsHeaders } from 'constants/tableHeaders'
+import { useFlights } from 'hooks/useFlights'
+import { useCallback, useEffect, useState } from 'react'
+import { getFormattedDate } from 'utils/getFormattedDate'
+import { getStorageFavoriteFlights } from 'services/storage'
+import { FlightDTO } from 'dtos/FlightDTO'
 
 export const FlightsFavorites = () => {
+  const { fetchGetAllFlights, flights } = useFlights()
+
+  const [favoriteFlights, setFavoriteFlights] = useState<FlightDTO[]>([])
+  const [maxPage, setMaxPage] = useState(0)
+
+  const defineFavoriteFlight = useCallback(() => {
+    const storageFavoriteFlights = getStorageFavoriteFlights()
+    const favoriteFlights = flights.filter((flight) =>
+      storageFavoriteFlights.includes(flight.id),
+    )
+    setFavoriteFlights(favoriteFlights)
+    setMaxPage(Math.ceil(favoriteFlights.length / 10))
+  }, [flights])
+
+  useEffect(() => {
+    fetchGetAllFlights()
+    defineFavoriteFlight()
+  }, [defineFavoriteFlight, fetchGetAllFlights])
+
   return (
     <FlightsFavoritesContainer>
       <Header title='Favoritos' />
@@ -12,40 +36,23 @@ export const FlightsFavorites = () => {
       <FlightsFindAllTable>
         <Table
           columns={flightsHeaders}
-          data={[
-            [
-              '1',
-              'São Paulo',
-              'Rio de Janeiro',
-              '10:00',
-              '12:00',
-              '2h',
-              'R$ 500,00',
-              'Avião X',
-            ],
-            [
-              '2',
-              'São Paulo',
-              'Belo Horizonte',
-              '14:00',
-              '16:00',
-              '2h',
-              'R$ 450,00',
-              'Avião Y',
-            ],
-            [
-              '3',
-              'São Paulo',
-              'Curitiba',
-              '18:00',
-              '20:00',
-              '2h',
-              'R$ 400,00',
-              'Avião Z',
-            ],
-          ]}
-          fetchData={() => {}}
-          maxPage={1}
+          data={favoriteFlights.map((flight) => {
+            return [
+              flight.id,
+              flight.flightNumber,
+              flight.airline,
+              flight.origin,
+              flight.destination,
+              getFormattedDate(flight.departure),
+              getFormattedDate(flight.arrival),
+              flight.price.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }),
+            ]
+          })}
+          fetchData={fetchGetAllFlights}
+          maxPage={maxPage}
         />
       </FlightsFindAllTable>
     </FlightsFavoritesContainer>
