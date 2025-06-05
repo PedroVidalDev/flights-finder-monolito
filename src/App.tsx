6c44ec48eda492c565da753bb729e012e965d663
PedroVidalDev <pedrohvidals@gmail.com>
2025-06-05 18:05:29 -0300

    feat: add react-toastify for notifications and implement notify utility function

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,21 @@ import { themes } from './themes'
 import { AppRoutes } from './Routes'
 
 import { SideBarContainer } from 'components/SideBar/styles'
+import { Bounce, ToastContainer } from 'react-toastify'
 
 function App() {
   return (
     <ThemeProvider theme={themes}>
+      <ToastContainer
+        position='top-center'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        pauseOnHover={false}
+        theme='light'
+        transition={Bounce}
+      />
       <SideBarContainer />
       <AppRoutes />
     </ThemeProvider>
